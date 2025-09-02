08f62bc5afcf011c83f286bb00137690d4c6e284
Ruslan Knyazev <knyazev12122003@mail.ru>
2025-09-02 14:03:28 +0300

    fixed config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,10 +2,12 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -18,48 +20,55 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:":8080"`
-	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	Address     string        `yaml:"address" env:"HTTP_ADDRESS" env-default:":8081"`
+	Timeout     time.Duration `yaml:"timeout" env:"HTTP_TIMEOUT" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env:"HTTP_IDLE_TIMEOUT" env-default:"60s"`
 }
 
 type DB struct {
-	Host     string `yaml:"host" env-default:"localhost"`
-	Port     int    `yaml:"port" env-default:"5433"`
-	User     string `yaml:"user" env-default:"postgres"`
-	Password string `yaml:"password" env-default:"postgres123"`
-	DBName   string `yaml:"dbname" env-default:"L0_db"`
-	SSLMode  string `yaml:"sslmode" env-default:"disable"`
+	Host     string `yaml:"host" env:"DB_HOST" env-default:"localhost"`
+	Port     int    `yaml:"port" env:"DB_PORT" env-default:"5433"`
+	User     string `yaml:"user" env:"DB_USER" env-default:"postgres"`
+	Password string `yaml:"password" env:"DB_PASSWORD"`
+	DBName   string `yaml:"dbname" env:"DB_NAME" env-default:"L0_db"`
+	SSLMode  string `yaml:"sslmode" env:"DB_SSLMODE" env-default:"disable"`
 }
 
 type Kafka struct {
-	Brokers  []string `yaml:"brokers"`
-	Topic    string   `yaml:"topic" env-default:"orders"`
-	GroupID  string   `yaml:"group_id" env-default:"orderservice"`
-	DLQTopic string   `yaml:"dlq_topic" env-default:"orders-dlq"`
-	ClientID string   `yaml:"client_id" env-default:"orderservice"`
+	Brokers  []string `yaml:"brokers" env:"KAFKA_BROKERS" env-separator:","`
+	Topic    string   `yaml:"topic" env:"KAFKA_TOPIC" env-default:"orders"`
+	GroupID  string   `yaml:"group_id" env:"KAFKA_GROUP_ID" env-default:"orderservice"`
+	DLQTopic string   `yaml:"dlq_topic" env:"KAFKA_DLQ_TOPIC" env-default:"orders-dlq"`
+	ClientID string   `yaml:"client_id" env:"KAFKA_CLIENT_ID" env-default:"orderservice"`
 }
 
 type Cache struct {
-	MaxEntries int `yaml:"max_entries" env-default:"1000"`
+	MaxEntries int `yaml:"max_entries" env:"CACHE_MAX_ENTRIES" env-default:"1000"`
 }
 
 type Log struct {
-	Level  string `yaml:"level" env-default:"info"`
-	Format string `yaml:"format" env-default:"text"`
+	Level  string `yaml:"level" env:"LOG_LEVEL" env-default:"info"`
+	Format string `yaml:"format" env:"LOG_FORMAT" env-default:"text"`
 }
 
 func Load() *Config {
-	configPath := "C:/Users/HEMi/Desktop/ex-L0/config/local.yaml"
+	_ = godotenv.Load(".env")
+	var cfg Config
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		configPath = "config/local.yaml"
+	}
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH environment variable is not set")
 	}
-
-	var cfg Config
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		log.Fatalf("Failed to read config file: %s", err)
 	}
 
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		log.Fatalf("read env: %v", err)
+	}
 	return &cfg
 }
